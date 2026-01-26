4eb86ea8ef7dc0d6d619d63dd7de159bf73ece93
saadeh73 <saadeh7380@gmail.com>
2026-01-26 02:49:50 +0200

    Update constants to adjust the network connection

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // constants.js - ثوابت مشروع MECO Wallet
 export const MECO_MINT = '7hBNyFfwYTv65z3ZudMAyKBw3BLMKxyKXsr5xM51Za4i';
 export const PROGRAM_ID = '97w8B9w7cFfdGjBNAyxsHBd8im6HZGbUrdvYwbSpsnG5';
-export const RPC_URL = 'https://api.devnet.solana.com';
+export const RPC_URL = 'https://api.mainnet-beta.solana.com';
 
 export const PRESALE_CONFIG = {
   MIN_SOL: 0.05,
